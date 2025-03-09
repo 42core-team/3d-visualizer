d4ee416ce7536e581f22430879f1011d1a929828
flomero <49640251+flomero@users.noreply.github.com>
2025-03-09 11:45:13 +0000

    grid

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
 	import { T, useTask } from '@threlte/core';
 	import { interactivity } from '@threlte/extras';
+	import { OrbitControls } from '@threlte/extras';
 	import { Spring } from 'svelte/motion';
+	import GrassBlock from './blocks/GrassBlock.svelte';
+	import { Sky } from '@threlte/extras';
 
 	interactivity();
 
@@ -11,7 +14,20 @@
 	useTask((delta) => {
 		rotation += delta;
 	});
+
+	// Generate positions for a 25x25 grid
+	const gridSize = 25;
+	const gridPositions: [number, number, number][] = [];
+
+	for (let x = 0; x < gridSize; x++) {
+		for (let z = 0; z < gridSize; z++) {
+			// Center the grid by offsetting by half the grid size
+			gridPositions.push([x - Math.floor(gridSize / 2), 0, z - Math.floor(gridSize / 2)]);
+		}
+	}
 </script>
+
+<Sky elevation={0.5} />
 
 <T.PerspectiveCamera
 	makeDefault
@@ -19,28 +35,20 @@
 	oncreate={(ref) => {
 		ref.lookAt(0, 1, 0);
 	}}
-/>
+>
+	<OrbitControls
+		autoRotate
+		enableZoom={false}
+		enableDamping
+		autoRotateSpeed={0.1}
+		enablePan={false}
+	/>
+</T.PerspectiveCamera>
 
 <T.DirectionalLight position={[0, 10, 10]} castShadow />
 
-<T.Mesh
-	rotation.y={rotation}
-	position.y={1}
-	scale={scale.current}
-	onpointerenter={() => {
-		scale.target = 1.5;
-	}}
-	onpointerleave={() => {
-		scale.target = 1;
-	}}
-	castShadow
-	position={[-0.3083, 1, 0.5945]}
->
-	<T.BoxGeometry args={[1, 2, 1]} />
-	<T.MeshStandardMaterial color="#ff0000" />
-</T.Mesh>
-
-<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
-	<T.CircleGeometry args={[4, 40]} />
-	<T.MeshStandardMaterial color="white" />
+<T.Mesh>
+	{#each gridPositions as position}
+		<GrassBlock {position} scale={scale.current} />
+	{/each}
 </T.Mesh>
---
+++ src/lib/components/blocks/GrassBlock.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { T } from '@threlte/core';
+	import { useTexture } from '@threlte/extras';
+	const texture = useTexture('textures/block/grass/top.png');
+	texture.then(() => {
+		console.log('texture has loaded');
+	});
+	$inspect($texture); // eventually a Three.Texture
+
+	let { position, scale } = $props();
+</script>
+
+{#await texture then map}
+	<T.Mesh {position} {scale}>
+		<T.BoxGeometry width={1} height={1} depth={1} />
+		<T.MeshBasicMaterial {map} />
+	</T.Mesh>
+{/await}
