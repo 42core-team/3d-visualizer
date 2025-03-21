d0b76270c5391608e053449f5272c588592611e9
flomero <49640251+flomero@users.noreply.github.com>
2025-03-21 12:29:03 +0000

    fix #12 and #14

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { T, useTask } from '@threlte/core';
+	import { T } from '@threlte/core';
 	import { interactivity } from '@threlte/extras';
 	import { OrbitControls } from '@threlte/extras';
 	import { Spring } from 'svelte/motion';
-	import GrassBlock from './blocks/GrassBlock.svelte';
 	import WallBlock from './blocks/WallBlock.svelte';
 	import Tree from './sprites/Tree.svelte';
 	import { Sky, CSM } from '@threlte/extras';
 	import Settings from './Settings.svelte';
 	import Unit from './sprites/Unit.svelte';
-	import { getStateAt, loadReplay, type TickObject } from '$lib/replayManager';
+	import {
+		getStateAt,
+		loadReplay,
+		getGameConfig,
+		type TickObject,
+		type GameConfig
+	} from '$lib/replayManager';
+	import Grid from './environment/Grid.svelte';
+	import type { Vector2Tuple } from 'three';
 
 	interactivity();
 
-	let billboarding = $state(false);
-	let fps = $state(30);
-
 	let props = $props();
+
+	let fps = $state(30);
+	let billboarding = $state(false);
 	let currFrame = $state<TickObject[]>([]);
 	let currentTick = $state(0);
 	let replayLoaded = $state(false);
+	let gameConfig = $state<GameConfig | undefined>(undefined);
+	let gridSize: Vector2Tuple = $derived.by(() => {
+		if (gameConfig) {
+			return [gameConfig.width, gameConfig.height];
+		}
+		return [0, 0];
+	});
 
 	console.log('Replay url:', props.replayUrl);
 
@@ -44,6 +58,7 @@
 			.then(() => {
 				console.info('Replay loaded!');
 				replayLoaded = true;
+				gameConfig = getGameConfig();
 				showFrame(0);
 			})
 			.catch((err) => {
@@ -53,29 +68,6 @@
 
 	const scale = new Spring(1);
 
-	// let rotation = 0;
-	// useTask((delta) => {
-	// 	rotation += delta;
-	// });
-
-	// Generate positions for a 25x25 grid
-	const gridSize = 25;
-	type BlockData = {
-		position: [number, number, number];
-		isDark: boolean;
-	};
-	const gridBlocks: BlockData[] = [];
-
-	for (let x = 0; x < gridSize; x++) {
-		for (let z = 0; z < gridSize; z++) {
-			const isDark = (x + z) % 2 === 0;
-
-			gridBlocks.push({
-				position: [x, 0, z],
-				isDark
-			});
-		}
-	}
 	$effect(() => {
 		showFrame(currentTick);
 	});
@@ -83,9 +75,9 @@
 
 <Sky elevation={1} />
 
-<T.PerspectiveCamera makeDefault position={[gridSize + 10, 10, gridSize + 10]} fov={60}>
+<T.PerspectiveCamera makeDefault position={[gridSize[0] + 10, 10, gridSize[1] + 10]} fov={60}>
 	<OrbitControls
-		target={[gridSize / 2, 0, gridSize / 2]}
+		target={[gridSize[0] / 2, 0, gridSize[1] / 2]}
 		autoRotate={false}
 		enableZoom={true}
 		enableDamping={true}
@@ -97,50 +89,47 @@
 
 {#if replayLoaded}
 	<Settings bind:billboarding bind:fps bind:currentTick />
-{/if}
 
-<CSM
-	args={{
-		mode: 'logarithmic'
-	}}
-	lightDirection={[-1, -1, -1]}
-	lightIntensity={5}
->
-	<T.Mesh position={[0, 0, 0]} recieveShadows>
-		{#each gridBlocks as block}
-			<GrassBlock position={block.position} scale={scale.current} isDark={block.isDark} />
-		{/each}
-	</T.Mesh>
+	<CSM
+		args={{
+			mode: 'logarithmic'
+		}}
+		lightDirection={[-1, -1, -1]}
+		lightIntensity={5}
+	>
+		<!-- Use the new Grid component for the grass blocks -->
+		<Grid position={[0, 0, 0]} size={gridSize} />
 
-	{#each currFrame as object}
-		{#if object.x != undefined && object.y != undefined}
-			{#if object.type === 0}
-				<Tree
-					position={[object.x, 1, object.y]}
-					treeType={object.teamId === 1 ? 'dead' : 'red'}
-					variant={1}
-					scale={[1, 1]}
-					{billboarding}
-				/>
-			{:else if object.type === 1}
-				<Unit
-					position={[object.x, 1, object.y]}
-					type_id={0}
-					team_id={object.teamId ? object.teamId : 0}
-					scale={[1, 1]}
-					{billboarding}
-				/>
-			{:else if object.type === 2}
-				<Tree
-					position={[object.x, 1, object.y]}
-					treeType="green"
-					variant={0}
-					scale={[1, 1]}
-					{billboarding}
-				/>
-			{:else}
-				<WallBlock position={[object.x, 0.75, object.y]} scale={scale.current} isDark={true} />
+		{#each currFrame as object}
+			{#if object.x != undefined && object.y != undefined}
+				{#if object.type === 0}
+					<Tree
+						position={[object.x, 1, object.y]}
+						treeType={object.teamId === 1 ? 'dead' : 'red'}
+						variant={1}
+						scale={[1, 1]}
+						{billboarding}
+					/>
+				{:else if object.type === 1}
+					<Unit
+						position={[object.x, 1, object.y]}
+						type_id={0}
+						team_id={object.teamId ? object.teamId : 0}
+						scale={[1, 1]}
+						{billboarding}
+					/>
+				{:else if object.type === 2}
+					<Tree
+						position={[object.x, 1, object.y]}
+						treeType="green"
+						variant={0}
+						scale={[1, 1]}
+						{billboarding}
+					/>
+				{:else}
+					<WallBlock position={[object.x, 0.75, object.y]} scale={scale.current} isDark={true} />
+				{/if}
 			{/if}
-		{/if}
-	{/each}
-</CSM>
+		{/each}
+	</CSM>
+{/if}
